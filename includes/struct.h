d2ae91c3249f11c53b084b3171a46023adb10be6
Colin Cauderlier <ccauderl@f2r9s12.clusters.42paris.fr>
2026-04-24 11:03:00 +0200

    New Makefile with obj files in separate folder

--- includes/struct.h
+++ includes/struct.h
@@ -6,8 +6,27 @@
 /*   By: ccauderl <ccauderl@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 14:25:48 by ccauderl          #+#    #+#             */
-/*   Updated: 2026/04/23 14:46:49 by ccauderl         ###   ########.fr       */
+/*   Updated: 2026/04/23 16:57:38 by ccauderl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+typedef struct s_token {
+	type	token_type;
+	char	*content;
+	t_token	*next;
+} t_token;
+
+typedef enum {
+	OPEN_PARENS;
+	CLOSING_PARENS;
+	SIMPLE_QUOTE;
+	DOUBLE_QUOTE;
+	PIPE;
+	REDIR_IN;
+	REDIR_OUT;
+	COMMAND;
+	FLAG;
+	DOLLAR;
+} type;
 
 
--- src/tokenizer/tokenize.c
+++ src/tokenizer/tokenize.c
@@ -6,14 +6,16 @@
 /*   By: ccauderl <ccauderl@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 14:44:46 by ccauderl          #+#    #+#             */
-/*   Updated: 2026/04/23 16:15:32 by ccauderl         ###   ########.fr       */
+/*   Updated: 2026/04/23 16:57:41 by ccauderl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/includes.h"
 #include "../includes/functions.h"
+#include "../includes/struct.h"
 
 void	tokenize(char *prompt)
 {
 	printf("%s\n", prompt);
+	
 }
