d6a712c2ef1cfda2ad147e86a6fe51ad7d8f6597
Lucie Nguyen <lucinguy@f2r8s20.clusters.42paris.fr>
2026-04-27 15:04:58 +0200

    corrected ft_putstr_fd.c in libft

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -6,28 +6,35 @@
 /*   By: lucinguy <lucinguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 17:17:08 by lucinguy          #+#    #+#             */
-/*   Updated: 2025/10/29 15:58:19 by lucinguy         ###   ########.fr       */
+/*   Updated: 2026/04/27 15:02:44 by lucinguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// size_t	ft_strlen(const char *s)
+// {
+// 	int	len;
+
+// 	len = 0;
+// 	while (s[len] != '\0')
+// 		len++;
+// 	return (len);
+// }
+
 void	ft_putstr_fd(char const *s, int fd)
 {
-	int	i;
+	int		i;
+	size_t	len;
 
 	i = 0;
 	if (!s)
 		return ;
-	while (s[i])
-	{
-		ft_putchar_fd(s[i], fd);
-		i++;
-	}
+	len = ft_strlen(s);
+	write(fd, s, len);
 }
-/*
-int	main(void)
-{
-	ft_putstr_fd("oui\n", 1);
-}
-*/
+
+// int	main(void)
+// {
+// 	ft_putstr_fd("oui\n", 1);
+// }
